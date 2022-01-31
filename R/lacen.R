e45f693a89ad70de7d9ec417ba6dc50ca08018f6
Leonardo Sanches <leonardo2.sanches@usp.br>
2022-01-31 14:08:05 -0300

    removed examples, added lacen.R with importfrom all packages. Use :: later.

---
+++ R/lacen.R
@@ -0,0 +1,7 @@
+#' lacen
+#'
+#' This package uses co-expression networks to find highly connected lncRNAs in RNAseq cancer data.
+#'
+#' @docType package
+
+#' @importFrom cluster ggplot2 gprofiler2 graphics grid limma org.Hs.eg.db rrvgo rtracklayer WGCNA
