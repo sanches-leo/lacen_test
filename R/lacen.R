d184cd193112e32b3191fafa52171c009578080b
Leonardo Sanches <leonardo2.sanches@usp.br>
2022-02-02 11:31:21 -0300

    Double colons in functions, fixed most of the notes. Global variable not defined is the only that remains, but it ocurs when I add a new colum to a df with a non defined variable, creating it.

--- R/lacen.R
+++ R/lacen.R
@@ -1,7 +1,12 @@
-#' lacen
-#'
-#' This package uses co-expression networks to find highly connected lncRNAs in RNAseq cancer data.
-#'
 #' @docType package
-
-#' @importFrom cluster ggplot2 gprofiler2 graphics grid limma org.Hs.eg.db rrvgo rtracklayer WGCNA
+#' @import cluster
+#' @import ggplot2
+#' @import gprofiler2
+#' @import graphics
+#' @import grid
+#' @import limma
+#' @import org.Hs.eg.db
+#' @import rrvgo
+#' @import rtracklayer
+#' @import WGCNA
+NULL
